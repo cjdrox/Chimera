475d6e104be2343c317d8dcd23661c44a89e530b
cjdrox <cjdrox@gmail.com>
2017-12-17 12:58:33 +0530

    DataAccessTest added

---
+++ Chimera.DataAccess.Test/BirthPlaceAdapterTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Chimera.DataAcess;
+using Chimera.Entities;
+using NUnit.Framework;
+
+namespace Chimera.DataAccess.Test
+{
+    [TestFixture]
+    public class BirthPlaceAdapterTest
+    {
+        [Test]
+        public void CreateTest()
+        {
+            const string connectionString = "";
+
+            var birthPlace = new BirthPlace
+            {
+                Id = 1,
+                CreatedAt = DateTime.Now,
+                ModifiedAt = DateTime.Now,
+                Name = "Test Place",
+                Region = null,
+            };
+
+            var bpAdapter = new BirthPlaceAdapter(connectionString);
+
+            bool insert = bpAdapter.Insert(birthPlace);
+            Assert.IsTrue(insert);
+        }
+    }
+}
